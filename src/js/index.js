9783a6aa567a73bcb008a61ffefcc2b2928a2e2c
ThePsyk3y <50011738+ThePsyk3y@users.noreply.github.com>
2020-11-03 02:36:28 +0530

    Base Main JS File

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
 import '../scss/style.scss';
+import APIController from './APIController';
+import UIController from './UIController';
+
+const Controller = (function () {
+  function getQuoteFromAPI() {
+    APIController.fetchFromServer()
+      .then((quoData) => {
+        const quotNum = Math.floor(Math.random() * 100);
+        return quoData[quotNum];
+      })
+      .then((quoteData) => {
+        UIController(quoteData.text, quoteData.author);
+      });
+  }
+  const setupEventListeners = function () {
+    document
+      .querySelector('.quote-change')
+      .addEventListener('click', getQuoteFromAPI);
+  };
+  return {
+    init() {
+      getQuoteFromAPI();
+      setupEventListeners();
+    },
+  };
+})(APIController, UIController);
+
+Controller.init();
